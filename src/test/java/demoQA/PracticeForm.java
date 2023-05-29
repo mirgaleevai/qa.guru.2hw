c4697f74f706790503911fe63d711b4c28b3b2c6
Mirgaleev Artur <easy0@inbox.ru>
2023-05-29 18:57:15 +0500

    added some cases

--- src/test/java/demoQA/PracticeForm.java
+++ src/test/java/demoQA/PracticeForm.java
@@ -1,11 +1,12 @@
 package demoQA;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class PracticeForm {
@@ -14,14 +15,21 @@ public class PracticeForm {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com/";
     }
 
     @Test
     void  RegistrationFormCheck () {
-        open("https://demoqa.com/automation-practice-form");
+        open("automation-practice-form");
 
-        $("#firstName").setValue("Oleg");
-        $("#lastName").setValue("Olegov");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $(By.xpath("//input[@id='firstName']")).setValue("Oleg");
+        $(By.xpath("//input[@id='lastName']")).setValue("Olegov");
+        $(By.xpath("//div[@id='userEmail-wrapper']//input")).setValue("Olegov@mail.ru");
+        $(By.xpath("//div[@id='genterWrapper']//label[contains(text(), 'Male')]")).click();
+        $(By.xpath("//div[@id='userNumber-wrapper']//input")).setValue("8888888888");
 
 
 
