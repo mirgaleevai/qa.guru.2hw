74ee0fce5549503e50a2d07d54682f5716f63404
Mirgaleev Artur <easy0@inbox.ru>
2023-05-30 15:57:35 +0500

    fixed class name, selectors, upload file

--- src/test/java/demoqa/studentRegistrationForm.java
+++ src/test/java/demoqa/studentRegistrationForm.java
@@ -1,19 +1,15 @@
-package demoQA;
+package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class StudentRegistrationForm {
+public class studentRegistrationForm {
 
     @BeforeAll
     static void beforeAll() {
@@ -23,17 +19,17 @@ public class StudentRegistrationForm {
     }
 
     @Test
-    void  RegistrationFormCheck () {
+    void registrationFormCheck() {
         open("/automation-practice-form");
 
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
-        $(By.xpath("//input[@id='firstName']")).setValue("Oleg");
-        $(By.xpath("//input[@id='lastName']")).setValue("Olegov");
-        $(By.xpath("//div[@id='userEmail-wrapper']//input")).setValue("Olegov@mail.ru");
-        $(By.xpath("//div[@id='genterWrapper']//label[contains(text(), 'Male')]")).click();
-        $(By.xpath("//div[@id='userNumber-wrapper']//input")).setValue("8888888888");
+        $("#firstName").setValue("Oleg");
+        $("#lastName").setValue("Olegov");
+        $("#userEmail").setValue("Olegov@mail.ru");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("8888888888");
 
         $(".react-datepicker__input-container").click();
         $(".react-datepicker__month-select").click();
@@ -42,11 +38,11 @@ public class StudentRegistrationForm {
         $(byText("1991")).click();
         $(byText("25")).click();
 
-        $(byXpath("//div[@id='subjectsWrapper']//input[@id='subjectsInput']")).setValue("E").pressEnter();
+        $("#subjectsWrapper #subjectsInput").setValue("E").pressEnter();
 
-        $(By.xpath("//div[@id='hobbiesWrapper']//label[contains(text(), 'Music')]")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
 
-        $("#uploadPicture").uploadFile(new File("src/test/resources/Wojak.png"));
+        $("#uploadPicture").uploadFromClasspath("Wojak.png");
         $("#currentAddress").setValue("Pushkina 123");
 
         $("#state").click();
@@ -62,28 +58,12 @@ public class StudentRegistrationForm {
         $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("8888888888"));
         $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("25 June,1991"));
         $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("English"));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("Wojak.png"));
         $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Pushkina 123"));
         $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
 
         $("#closeLargeModal").click();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
