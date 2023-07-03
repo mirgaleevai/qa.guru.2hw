e35b68b20131bd907a975f2b4694e0ad589ae741
Mirgaleev Artur <easy0@inbox.ru>
2023-07-03 20:13:04 +0500

    attachments added

--- src/test/java/demoqa/TestBaseRemote.java
+++ src/test/java/demoqa/TestBaseRemote.java
@@ -2,9 +2,12 @@ package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqa.helpers.Attach;
 import demoqa.pages.RegistrationPage;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -30,6 +33,19 @@ public class TestBaseRemote {
 
         Configuration.browserCapabilities = capabilities;
 
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
